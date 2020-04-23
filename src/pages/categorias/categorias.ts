a1ab8ed685f898e29d4d90fade46d54ff2f58dfc
marcosemilson <marcosenpereira@gmail.com>
2020-04-23 13:45:00 -0300

    Mostrando categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the CategoriasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -15,11 +11,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  bucketUrl: String = API_CONFIG.bucketBaseUrl;
+
+  items: CategoriaDTO[];
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public categoraService: CategoriaService) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
+    this.categoraService.findALL()
+    .subscribe(response=>{
+      this.items = response;
+    },
+    error=>{
+      console.log(error);
+    });
+    
+
+
+    
   }
 
 }
